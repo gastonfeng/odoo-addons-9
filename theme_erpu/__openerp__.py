aa2db844bfbd67dd47f659096538b7ea617de487
Bogdan Lisnenko <lem8r.ka11a@gmail.com>
2016-04-19 17:41:43 +0300

    [IMP] theme_erpu: new theme

---
+++ theme_erpu/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'ERP Ukraine theme',
+    'description': 'Theme for erp.co.ua site.',
+    'version': '1.0',
+    'author': 'Bohdan Lisnenko',
+
+    'data': [
+        'views/templates.xml'
+    ],
+    'depends': [
+        'website',
+        'website_blog',
+    ],
+}
