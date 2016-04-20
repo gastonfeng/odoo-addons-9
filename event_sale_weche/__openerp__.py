808e7c3010f3d3544a51b0fc5bc89500cc78e32f
Bogdan Lisnenko <lem8r.ka11a@gmail.com>
2016-04-20 18:51:25 +0300

    [IMP] event_sale_weche: bage desing tweaks

---
+++ event_sale_weche/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Event Sale weche',
+    'description': 'Sale tickets tweaks',
+    'version': '1.0',
+    'author': 'Bohdan Lisnenko',
+    'website': "https://erp.co.ua",
+    'data': [
+        'views/views.xml'
+    ],
+    'depends': [
+        'event',
+        'event_sale',
+    ],
+}
