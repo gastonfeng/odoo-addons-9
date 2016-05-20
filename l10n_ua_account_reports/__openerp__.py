5e477fee5cf1aa1a03a4d703057b3044fbc4cb74
Bogdan Lisnenko <lem8r.ka11a@gmail.com>
2016-05-20 17:49:35 +0300

    [IMP] account_reports: new ua Reports

---
+++ l10n_ua_account_reports/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Ukrainian Accounting Reports',
+    'author': 'ERP Ukraine',
+    'website': 'https://erp.co.ua',
+    'category': 'Localization/Account Charts',
+    'depends': ['account'],
+    'version': '1.0',
+    'description': """
+Друковані форми первинних документів для бухгалтера
+======================================================
+Цей модуль встановлює додаткові форми для друку
+первинних складських документів з інтерфейсу бухгалтера.
+
+В меню друку рахунків можна буде вибрати форму накладних,
+що відповідє вимогам Українських стандартів.
+""",
+    'auto_install': False,
+    'demo': [],
+    'data': ['views/acc_report_nakladna.xml',
+             'data/account_report.xml'],
+    'installable': True
+}
