55ae2703ff59a22a336b5ca42b8de937a99ceb12
Bogdan Lisnenko <lem8r.ka11a@gmail.com>
2016-06-15 13:51:05 +0300

    [IMP] p24b_sync: get acc balance on sttmnt import

--- account_bank_sync_p24b/__openerp__.py
+++ account_bank_sync_p24b/__openerp__.py
@@ -3,13 +3,13 @@
     'name': 'PrivatBank online synchronization',
     'author': 'ERP Ukraine',
     'website': 'https://erp.co.ua',
-    'summary': u"Онлайн синхронізація з ПриватБанком",
+    'summary': u"Sync statements with PrivatBank online",
     'category': 'Accounting & Finance',
     'depends': ['account'],
     'version': '1.0',
     'description': """
-Цей додаток дозволяє синхронізувати виписку та платежі
-з ПриватБанком у режимі онлайн.
+This app enables online synchronization of
+bank statement and payments with PrivatBank.
 """,
     'auto_install': False,
     'demo': [],
