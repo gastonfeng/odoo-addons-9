d2dc8529ddfca9c705511353c745da176e93dadb
Bogdan Lisnenko <lem8r.ka11a@gmail.com>
2016-08-01 14:03:43 +0300

    [IMP] hr_payroll: take analytic from contact

--- l10n_ua_hr_payroll_account/hr_payroll_account.py
+++ l10n_ua_hr_payroll_account/hr_payroll_account.py
@@ -70,6 +70,15 @@ class hr_payslip(osv.osv):
                 'date': date,
             }
             for line in slip.details_by_salary_rule_category:
+                an_acc_id = False
+
+                if line.salary_rule_id.analytic_account_id:
+                    cont = slip.contract_id or False
+                    if cont and cont.analytic_account_id:
+                        an_acc_id = cont.analytic_account_id.id
+                    else:
+                        an_acc_id = line.salary_rule_id.analytic_account_id.id
+
                 amt = slip.credit_note and -line.total or line.total
                 if float_is_zero(amt, precision_digits=precision):
                     continue
@@ -90,10 +99,7 @@ class hr_payslip(osv.osv):
                         'date': date,
                         'debit': amt > 0.0 and amt or 0.0,
                         'credit': amt < 0.0 and -amt or 0.0,
-                        'analytic_account_id':      # TODO: take from contract
-                            (line.salary_rule_id.analytic_account_id and
-                                line.salary_rule_id.analytic_account_id.id or
-                                False),
+                        'analytic_account_id': an_acc_id,
                         'tax_line_id':
                             (line.salary_rule_id.account_tax_id and
                                 line.salary_rule_id.account_tax_id.id or
