fd959c21d5f5cb068b3c018c9a7569033b7f029e
Bogdan Lisnenko <lem8r.ka11a@gmail.com>
2016-08-29 14:56:40 +0300

    [NEW] ukrainian national payroll rules

---
+++ l10n_ua_hr_payroll_account_psbo/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Ukrainian - Payroll with Accounting (PSBO)',
+    'author': "ERP Ukraine",
+    'website': "https://erp.co.ua",
+    'category': 'Localization',
+    'depends': ['l10n_ua_hr_payroll', 'hr_payroll_account'],
+    'version': '1.2',
+    'price': 100.00,
+    'currency': 'EUR',
+    'description': """
+Бухгалтерські проведення для зарплати (ПСБО)
+==============================================
+    """,
+
+    'auto_install': True,
+    # 'website': 'https://www.odoo.com/page/accounting',
+    'demo': [],
+    'data': [
+        'l10n_ua_hr_payroll_account_data.xml',
+    ],
+    # 'post_init_hook': '_set_accounts',
+    'installable': True
+}
