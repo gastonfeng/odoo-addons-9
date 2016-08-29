1756331ed8af4a66e2a675259990c48ce13c5c7e
Bogdan Lisnenko <lem8r.ka11a@gmail.com>
2016-08-29 13:19:40 +0300

    [NEW] l10n_ua_psbo: ukrainian national accounting

---
+++ l10n_ua_psbo/__openerp__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name': u"Ukraine - Accounting PSBO",
+    'author': "ERP Ukraine",
+    'website': "https://erp.co.ua",
+    'summary': u"Український бухоблік згідно ПСБО",
+    'version': '1.1',
+    'price': 100.00,
+    'currency': 'EUR',
+    'description': u"""
+Бухгалтерський облік для України (ПСБО)
+=======================================
+
+Цей модуль дає можливість вести бухгалтерський
+облік діяльності підприємства згідно національних
+стандартів фінансової звітності.
+
+Встановлюється типовий план рахунків,
+який підійде більшості підприємств.
+Також буде створено налаштування податків
+для обліку ПДВ
+    """,
+    'category': 'Localization/Account Charts',
+    'version': '1.1',
+    'depends': ['account'],
+    'data': [
+        'partner_view.xml',
+        'data/account_chart_template.xml',
+        'data/account.account.template.csv',
+        'data/account_tax_template.xml',
+        'data/account_chart_template_config.xml',
+        'data/account_chart_template.yml',
+    ],
+    'installable': True,
+}
