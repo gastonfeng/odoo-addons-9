cddead0cb584993f25a5b65070a91c66ef0e508d
Bogdan Lisnenko <lem8r.ka11a@gmail.com>
2017-02-09 15:50:04 +0200

    [IMP] contacts mgmt

---
+++ sale_team_own_contacts/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Sales team see own contacts',
+    'description': '''
+        Sales user will see own contacts only''',
+    'author': 'ERP Ukraine',
+    'website': 'https://erp.co.ua',
+    'support': 'support@erp.co.ua',
+    'license': 'AGPL-3',
+    'category': 'Sales',
+    'version': '0.1',
+    'depends': ['sales_team'],
+    'data': [
+        'security/sales_team_security.xml',
+    ],
+}
