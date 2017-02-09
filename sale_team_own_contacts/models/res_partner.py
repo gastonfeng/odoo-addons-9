8f07fd3cbd541e97a7fd44e288b999797eb3f2c5
Bogdan Lisnenko <lem8r.ka11a@gmail.com>
2017-02-09 22:41:34 +0200

    [IMP] autofill Salesperson as current user

---
+++ sale_team_own_contacts/models/res_partner.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models
+
+
+class PartnerST(models.Model):
+    _inherit = 'res.partner'
+    user_id = fields.Many2one(
+        'res.users',
+        string='Salesperson',
+        default=lambda self: self.env.user or False,
+        help='''The internal user that is in charge of
+                communicating with this contact if any.''')
