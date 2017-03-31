f22e2c9f73d3019dddf33eb7d06501e7e79c5dcf
Bogdan Lisnenko <lem8r.ka11a@gmail.com>
2017-03-31 19:21:56 +0300

    [IMP] allow see own company

--- sale_team_own_contacts/models/res_partner.py
+++ sale_team_own_contacts/models/res_partner.py
@@ -5,9 +5,15 @@ from odoo import fields, models
 
 class PartnerST(models.Model):
     _inherit = 'res.partner'
+
     user_id = fields.Many2one(
         'res.users',
         string='Salesperson',
         default=lambda self: self.env.user or False,
         help='''The internal user that is in charge of
                 communicating with this contact if any.''')
+
+    my_company_ids = fields.One2many(
+        'res.company',
+        'partner_id',
+        string='My Company')
